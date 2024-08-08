fe0a2960c5c419c486b1c5b110bb87246d09f03a
fathurhayokerja <fathur@hayokerja.com>
2024-08-08 08:22:28 +0700

    feat: adding container component and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <main className='flex'>
-      <h1 className='tulisan'>Test</h1>
-    </main>
-  );
+  return <main className='flex'></main>;
 }
